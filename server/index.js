76248ccc3f93b54038d7783b8ed794499bedef79
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-02-14 21:11:28 -0500

    Create index.ejs file and configure express server to read the views folder

--- server/index.js
+++ server/index.js
@@ -2,14 +2,26 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Task Manager" });
+});
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 
