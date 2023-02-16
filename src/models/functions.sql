35b0e1a64ff449a55863727905454de55281d2a8
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-02-15 23:20:03 -0500

    Change some column names of the tables and fix some stored procedures

--- src/models/functions.sql
+++ src/models/functions.sql
@@ -1,24 +1,25 @@
 -- Creates a new task
-CREATE OR REPLACE PROCEDURE create_task (user_id INTEGER)
+CREATE OR REPLACE PROCEDURE create_task (id INTEGER, new_title VARCHAR(25), new_description VARCHAR(100), new_status VARCHAR(25))
 AS $$
     BEGIN
-        INSERT INTO task(user_id, title, description, status);
+        INSERT INTO task(user_id, task_title, task_description, task_status)
+        VALUES (id, new_title, new_description, new_status);
     END;
 $$ LANGUAGE plpgsql;
 
 -- Updates the title of the task
-CREATE OR REPLACE PROCEDURE update_task_title (task_id INTEGER, new_title VARCHAR(25))
+CREATE OR REPLACE PROCEDURE update_task_title (id INTEGER, new_title VARCHAR(25))
 AS $$
     BEGIN
-        UPDATE task SET title = new_title  WHERE id = task_id;
+        UPDATE task SET task_title = new_title  WHERE task_id = id;
     END;
 $$ LANGUAGE plpgsql;
 
 -- Updates the description of the task
-CREATE OR REPLACE PROCEDURE update_task_description (task_id INTEGER, new_description VARCHAR(100))
+CREATE OR REPLACE PROCEDURE update_task_description (id INTEGER, new_description VARCHAR(100))
 AS $$
     BEGIN
-        UPDATE task SET description = new_description  WHERE id = task_id;
+        UPDATE task SET task_description = new_description  WHERE task_id = id;
     END;
 $$ LANGUAGE plpgsql;
 
@@ -26,7 +27,8 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION set_old_task() RETURNS trigger
 AS $$
     BEGIN
-        INSERT INTO old_task(user_id, title, description, status) VALUES (OLD.user_id, OLD.title, OLD.description, OLD.status);
+        INSERT INTO old_task(user_id, task_title, task_description, task_status)
+        VALUES (OLD.user_id, OLD.task_title, OLD.task_description, OLD.task_status);
         RETURN OLD;
     END;
 $$ LANGUAGE plpgsql;
