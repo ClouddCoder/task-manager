a90c160a81260e4a586dcf0c5dd8a76fbc8a0675
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-02-16 22:45:28 -0500

    Set jwt to authenticate the user

--- src/controllers/users-controllers.js
+++ src/controllers/users-controllers.js
@@ -1,14 +1,27 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import pool from "../db.js";
+
+const jwtPassword = process.env.JWT_SECRET;
 
 const register = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  const registerQuery = "CALL register($1, $2, $3)";
+  const registerQuery = "SELECT * FROM register($1, $2, $3)";
+
+  const passwordHash = await bcrypt.hash(password, 10);
 
   try {
-    await pool.query(registerQuery, [username, email, password]);
+    const userId = await pool.query(registerQuery, [username, email, passwordHash]);
 
-    return res.json({ message: "User registered successfully" });
+    const payload = {
+      userId: userId.rows[0].register,
+      username,
+    };
+
+    const token = jwt.sign(payload, jwtPassword);
+
+    return res.json({ message: "User registered successfully", token });
   } catch (error) {
     next(error);
   }
--- src/models/user-functions.sql
+++ src/models/user-functions.sql
@@ -1,7 +1,13 @@
 -- Register a new user
-CREATE OR REPLACE PROCEDURE register ( new_username VARCHAR(25), new_email VARCHAR(40), new_password VARCHAR(100))
+CREATE OR REPLACE FUNCTION register (new_username VARCHAR(25), new_email VARCHAR(40), new_password VARCHAR(100))
+RETURNS integer
 AS $$
+DECLARE
+    id integer;
 BEGIN
-    INSERT INTO user_data (username, user_email, user_password) VALUES (new_username, new_email, new_password);
+    INSERT INTO user_data (username, user_email, user_password) VALUES (new_username, new_email, new_password)
+        RETURNING user_id INTO id;
+
+    RETURN id;
 END;
 $$ LANGUAGE plpgsql;
