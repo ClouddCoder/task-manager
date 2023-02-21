5362e6e17a3e6eb3426cbd71baeaa2f1669604fe
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-02-20 22:31:57 -0500

    Create method to fetch the tasks when the user log in

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import router from "./routes/index.js";
 
-dotenv.config();
+// dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,18 +16,16 @@ const app = express();
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "Task Manager" });
-});
-
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, "public")));
+app.use(express.static(join(__dirname, "scripts")));
 app.use(router);
 
 // eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => res.status(404).json({ message: "Informacion invalida" }));
+// app.use((err, req, res, next) => res.status(404).json({ message: "Informacion invalida" }));
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 
---
+++ src/scripts/login.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".login-form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const init = {
+    method: "POST",
+    body: JSON.stringify({ email: email.value, password: password.value }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("/login", init)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error("Something went wrong");
+    })
+    .then((data) => {
+      window.localStorage.setItem("logged", JSON.stringify(data));
+      window.location.href = "/";
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+});
